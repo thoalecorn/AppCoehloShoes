0332591a0aed6421b5c276f13721f22e9f94933b
Thomas Cornejo <cuc20131002@mail.udes.edu.co>
2026-03-13 01:20:24 -0500

    feat: integrate Storefront API

---
+++ js/config-storefront.js
@@ -0,0 +1,14 @@
+const SHOPIFY_STOREFRONT_CONFIG = {
+  storeName: 'coelho-9559',
+  storefrontAccessToken: 'd7ef96dd54c658042910f43856d789d7',
+  apiVersion: '2026-01',
+  
+  settings: {
+    productsPerPage: 50,
+    enableCache: true,
+    cacheLifetime: 30,
+    preferredLanguage: 'es'
+  }
+};
+
+window.SHOPIFY_STOREFRONT_CONFIG = SHOPIFY_STOREFRONT_CONFIG;
